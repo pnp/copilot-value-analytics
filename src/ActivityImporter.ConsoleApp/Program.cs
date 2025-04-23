f0becb9c8ab7bdd1d3e937086d2cea87eb2fdb3a
Sam Betts <sambetts@microsoft.com>
2025-04-23 14:59:38 +0200

    +App Insights for importer

--- src/ActivityImporter.ConsoleApp/Program.cs
+++ src/ActivityImporter.ConsoleApp/Program.cs
@@ -3,10 +3,14 @@ using Common.DataUtils.Config;
 using Common.Engine.Config;
 using Entities.DB;
 using Entities.DB.DbContexts;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.ApplicationInsights.Channel;
+using Common.Engine;
 
 var builder = new ConfigurationBuilder()
     .AddEnvironmentVariables()
@@ -30,30 +34,58 @@ catch (ConfigurationMissingException ex)
     throw;
 }
 
-
-var logger = LoggerFactory.Create(config =>
+using var channel = new InMemoryChannel();
+try
 {
-    config.AddConsole();
-}).CreateLogger("Import console");
-
-
-Console.WriteLine("Starting import job");
-
-var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-optionsBuilder.UseSqlServer(config.ConnectionStrings.SQL);
-
-using (var db = new DataContext(optionsBuilder.Options))
-{
-    await DbInitialiser.EnsureInitialised(db, logger, config.TestUPN);
-
-    if (config.DevMode)
+    var services = new ServiceCollection();
+    services.Configure<TelemetryConfiguration>(tConfig => tConfig.TelemetryChannel = channel);
+    services.AddLogging(builder =>
     {
-        await FakeDataGen.GenerateFakeActivityForAllUsers(db, logger);
+        builder.AddConsole();
+
+        if (config.AppInsightsConnectionString != null)
+        {
+            builder.AddApplicationInsights(
+                configureTelemetryConfiguration: (c) => c.ConnectionString = config.AppInsightsConnectionString,
+                configureApplicationInsightsLoggerOptions: (options) => { }
+            );
+        }
+        else
+        {
+            Console.WriteLine("No Application Insights connection string found. " +
+                "Telemetry will not be sent to Application Insights.");
+        }
+    });
+
+    var serviceProvider = services.BuildServiceProvider();
+    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation($"Importer {VersionConstants.Version} starting import job");
+
+    var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+    optionsBuilder.UseSqlServer(config.ConnectionStrings.SQL);
+
+    using (var db = new DataContext(optionsBuilder.Options))
+    {
+        await DbInitialiser.EnsureInitialised(db, logger, config.TestUPN);
+
+        if (config.DevMode)
+        {
+            await FakeDataGen.GenerateFakeActivityForAllUsers(db, logger);
+        }
+
+        // Import things
+        var t = new ProgramTasks(config, logger);
+        await t.DownloadAndSaveActivityData();
+
+        await t.GetGraphTeamsAndUserData();
     }
+}
+finally
+{
+    // Explicitly call Flush() followed by Delay, as required in console apps.
+    // This ensures that even if the application terminates, telemetry is sent to the back end.
+    channel.Flush();
 
-    // Import things
-    var t = new ProgramTasks(config, logger);
-    await t.DownloadAndSaveActivityData();
-
-    await t.GetGraphTeamsAndUserData();
+    await Task.Delay(TimeSpan.FromMilliseconds(1000));
 }
